b0e5b4fc898cccc70e31e85720421b7c2d6e9ada
mukhamed-Ali <mailsmorales21@mail.ru>
2022-07-17 04:12:53 +0600

    npm-i-uuid

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -19,7 +19,7 @@ const Project = () => {
   return (
     <div className="project-details">
       <ProjectSummary project={document} />
-      <ProjectComments />
+      <ProjectComments project={document} />
     </div>
   );
 };
--- src/pages/project/ProjectComments.jsx
+++ src/pages/project/ProjectComments.jsx
@@ -1,12 +1,14 @@
 import { serverTimestamp } from "firebase/firestore";
 import React, { useState } from "react";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
 
-const ProjectComments = () => {
+const ProjectComments = ({ project }) => {
   const { user } = useAuthContext();
+  const { updateDocument, response } = useCollection("projects");
   const [newComment, setNewComment] = useState("");
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     const commentToAdd = {
       displayName: user.displayName, // Имя Автора
       photoURL: user.photoURL, // Фотка Автора
@@ -14,11 +16,14 @@ const ProjectComments = () => {
       createdAt: serverTimestamp(),
       id: Math.random(),
     };
+
+    await updateDocument(project.id, {
+      comments: [...project.comments, commentToAdd],
+    });
   };
   return (
     <div className="project-comments">
       <h4>Комментарии к проекту</h4>
-
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
           <span>Описание:</span>
