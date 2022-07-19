1855487520f9c0334f54b93a5941e9a58c6c3984
mukhamed-Ali <mailsmorales21@mail.ru>
2022-07-19 12:30:36 +0600

    add-function-filter

--- src/pages/dashboard/ProjectFilter.jsx
+++ src/pages/dashboard/ProjectFilter.jsx
@@ -9,21 +9,21 @@ const filterList = [
   "sales",
 ];
 
-export const ProjectFilter = () => {
-  const [currentFilter, setCurrentFilter] = useState("all");
-
-  const handleClick = (newFilter) => {
-    setCurrentFilter(newFilter);
-  };
+export const ProjectFilter = ({ handleClick, currentFilter }) => {
   return (
     <div className="project-filter">
       <nav>
         <p>Filter by:</p>
         {filterList.map((filter) => (
-          <button key={filter}>{filter}</button>
+          <button
+            key={filter}
+            className={currentFilter === filter ? "active" : ""}
+            onClick={() => handleClick(filter)}
+          >
+            {filter}
+          </button>
         ))}
       </nav>
     </div>
   );
 };
-
